e5b52a309260da7ed8333fc6e502cb8bf1b41353
Izan <[ios2asir@gmail.com]>
2017-11-29 13:21:34 +0100

    Arreglado el problema de printf

--- src/TestPendulo.java
+++ src/TestPendulo.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class TestPendulo {
 	public static void main (String[] args) {
 		
-		System.out.println("Longitud");
+		System.out.println("Introduzca longitud: ");
 		Scanner sc = new Scanner(System.in);
 		
 		double longitud = sc.nextDouble();
 		sc.close();
 		
 		double periodo=Pendulo.calcularPeriodo(longitud);
-		System.out.println("Longitud: "+longitud+"- periodo: "+periodo);
+		System.out.printf("Longitud del péndulo: %f tiene un periodo de oscilación de: %f",longitud,periodo);
 	}
 }
